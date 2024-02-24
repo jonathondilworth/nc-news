b72d28edf89e82ca4e8b47ab9e355865f546c334
Jon Dilworth <jon@dilworth.dev>
2024-02-24 00:44:50 +0000

    added functionality for getting user by username via /api/users/:username

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,9 +1,18 @@
-const { selectUsers } = require("../models/users.model");
+const { selectUsers, selectUserByUsername } = require("../models/users.model");
 
 exports.getUsers = (request, response, next) => {
     return selectUsers()
     .then((result) => {
         response.status(200).send({ users: result });
+    })
+    .catch(next);
+};
+
+exports.getUser = (request, response, next) => {
+    const { username } = request.params;
+    return selectUserByUsername(username)
+    .then((result) => {
+        response.status(200).send({ user: result });
     })
     .catch(next);
 };
